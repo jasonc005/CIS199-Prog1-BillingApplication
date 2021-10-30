5adca4a4a89f6905c9c3e77a41516b7e88f28b00
jasonc005 <jason.chandlerp@gmail.com>
2021-10-30 16:24:36 -0400

    Add project files.

---
+++ BillingApplication/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace BillingApplication
+{
+    partial class BillingApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.descriptionLabel = new System.Windows.Forms.Label();
+            this.priceLabel = new System.Windows.Forms.Label();
+            this.quantityLabel = new System.Windows.Forms.Label();
+            this.descriptionBox1 = new System.Windows.Forms.TextBox();
+            this.descriptionBox2 = new System.Windows.Forms.TextBox();
+            this.descriptionBox3 = new System.Windows.Forms.TextBox();
+            this.descriptionBox4 = new System.Windows.Forms.TextBox();
+            this.descriptionBox5 = new System.Windows.Forms.TextBox();
+            this.priceBox1 = new System.Windows.Forms.TextBox();
+            this.priceBox2 = new System.Windows.Forms.TextBox();
+            this.priceBox3 = new System.Windows.Forms.TextBox();
+            this.priceBox4 = new System.Windows.Forms.TextBox();
+            this.priceBox5 = new System.Windows.Forms.TextBox();
+            this.quantityBox1 = new System.Windows.Forms.TextBox();
+            this.quantityBox2 = new System.Windows.Forms.TextBox();
+            this.quantityBox3 = new System.Windows.Forms.TextBox();
+            this.quantityBox4 = new System.Windows.Forms.TextBox();
+            this.quantityBox5 = new System.Windows.Forms.TextBox();
+            this.calculateTotalButton = new System.Windows.Forms.Button();
+            this.totalBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // descriptionLabel
+            // 
+            this.descriptionLabel.AutoSize = true;
+            this.descriptionLabel.Location = new System.Drawing.Point(12, 33);
+            this.descriptionLabel.Name = "descriptionLabel";
+            this.descriptionLabel.Size = new System.Drawing.Size(100, 13);
+            this.descriptionLabel.TabIndex = 0;
+            this.descriptionLabel.Text = "Product Description";
+            // 
+            // priceLabel
+            // 
+            this.priceLabel.AutoSize = true;
+            this.priceLabel.Location = new System.Drawing.Point(232, 33);
+            this.priceLabel.Name = "priceLabel";
+            this.priceLabel.Size = new System.Drawing.Size(71, 13);
+            this.priceLabel.TabIndex = 1;
+            this.priceLabel.Text = "Product Price";
+            // 
+            // quantityLabel
+            // 
+            this.quantityLabel.AutoSize = true;
+            this.quantityLabel.Location = new System.Drawing.Point(373, 33);
+            this.quantityLabel.Name = "quantityLabel";
+            this.quantityLabel.Size = new System.Drawing.Size(86, 13);
+            this.quantityLabel.TabIndex = 2;
+            this.quantityLabel.Text = "Product Quantity";
+            // 
+            // descriptionBox1
+            // 
+            this.descriptionBox1.Location = new System.Drawing.Point(15, 69);
+            this.descriptionBox1.Name = "descriptionBox1";
+            this.descriptionBox1.Size = new System.Drawing.Size(178, 20);
+            this.descriptionBox1.TabIndex = 3;
+            // 
+            // descriptionBox2
+            // 
+            this.descriptionBox2.Location = new System.Drawing.Point(15, 107);
+            this.descriptionBox2.Name = "descriptionBox2";
+            this.descriptionBox2.Size = new System.Drawing.Size(178, 20);
+            this.descriptionBox2.TabIndex = 4;
+            // 
+            // descriptionBox3
+            // 
+            this.descriptionBox3.Location = new System.Drawing.Point(15, 143);
+            this.descriptionBox3.Name = "descriptionBox3";
+            this.descriptionBox3.Size = new System.Drawing.Size(178, 20);
+            this.descriptionBox3.TabIndex = 5;
+            // 
+            // descriptionBox4
+            // 
+            this.descriptionBox4.Location = new System.Drawing.Point(15, 179);
+            this.descriptionBox4.Name = "descriptionBox4";
+            this.descriptionBox4.Size = new System.Drawing.Size(178, 20);
+            this.descriptionBox4.TabIndex = 6;
+            // 
+            // descriptionBox5
+            // 
+            this.descriptionBox5.Location = new System.Drawing.Point(15, 215);
+            this.descriptionBox5.Name = "descriptionBox5";
+            this.descriptionBox5.Size = new System.Drawing.Size(178, 20);
+            this.descriptionBox5.TabIndex = 7;
+            // 
+            // priceBox1
+            // 
+            this.priceBox1.Location = new System.Drawing.Point(235, 69);
+            this.priceBox1.Name = "priceBox1";
+            this.priceBox1.Size = new System.Drawing.Size(100, 20);
+            this.priceBox1.TabIndex = 8;
+            // 
+            // priceBox2
+            // 
+            this.priceBox2.Location = new System.Drawing.Point(235, 107);
+            this.priceBox2.Name = "priceBox2";
+            this.priceBox2.Size = new System.Drawing.Size(100, 20);
+            this.priceBox2.TabIndex = 9;
+            // 
+            // priceBox3
+            // 
+            this.priceBox3.Location = new System.Drawing.Point(235, 143);
+            this.priceBox3.Name = "priceBox3";
+            this.priceBox3.Size = new System.Drawing.Size(100, 20);
+            this.priceBox3.TabIndex = 10;
+            // 
+            // priceBox4
+            // 
+            this.priceBox4.Location = new System.Drawing.Point(235, 179);
+            this.priceBox4.Name = "priceBox4";
+            this.priceBox4.Size = new System.Drawing.Size(100, 20);
+            this.priceBox4.TabIndex = 11;
+            // 
+            // priceBox5
+            // 
+            this.priceBox5.Location = new System.Drawing.Point(235, 215);
+            this.priceBox5.Name = "priceBox5";
+            this.priceBox5.Size = new System.Drawing.Size(100, 20);
+            this.priceBox5.TabIndex = 12;
+            // 
+            // quantityBox1
+            // 
+            this.quantityBox1.Location = new System.Drawing.Point(376, 69);
+            this.quantityBox1.Name = "quantityBox1";
+            this.quantityBox1.Size = new System.Drawing.Size(100, 20);
+            this.quantityBox1.TabIndex = 13;
+            // 
+            // quantityBox2
+            // 
+            this.quantityBox2.Location = new System.Drawing.Point(376, 107);
+            this.quantityBox2.Name = "quantityBox2";
+            this.quantityBox2.Size = new System.Drawing.Size(100, 20);
+            this.quantityBox2.TabIndex = 14;
+            // 
+            // quantityBox3
+            // 
+            this.quantityBox3.Location = new System.Drawing.Point(376, 143);
+            this.quantityBox3.Name = "quantityBox3";
+            this.quantityBox3.Size = new System.Drawing.Size(100, 20);
+            this.quantityBox3.TabIndex = 15;
+            // 
+            // quantityBox4
+            // 
+            this.quantityBox4.Location = new System.Drawing.Point(376, 179);
+            this.quantityBox4.Name = "quantityBox4";
+            this.quantityBox4.Size = new System.Drawing.Size(100, 20);
+            this.quantityBox4.TabIndex = 16;
+            // 
+            // quantityBox5
+            // 
+            this.quantityBox5.Location = new System.Drawing.Point(376, 215);
+            this.quantityBox5.Name = "quantityBox5";
+            this.quantityBox5.Size = new System.Drawing.Size(100, 20);
+            this.quantityBox5.TabIndex = 17;
+            // 
+            // calculateTotalButton
+            // 
+            this.calculateTotalButton.Location = new System.Drawing.Point(190, 258);
+            this.calculateTotalButton.Name = "calculateTotalButton";
+            this.calculateTotalButton.Size = new System.Drawing.Size(125, 23);
+            this.calculateTotalButton.TabIndex = 18;
+            this.calculateTotalButton.Text = "Calculate Total Price";
+            this.calculateTotalButton.UseVisualStyleBackColor = true;
+            this.calculateTotalButton.Click += new System.EventHandler(this.calculateTotalButton_Click);
+            // 
+            // totalBox
+            // 
+            this.totalBox.Location = new System.Drawing.Point(190, 302);
+            this.totalBox.Name = "totalBox";
+            this.totalBox.ReadOnly = true;
+            this.totalBox.Size = new System.Drawing.Size(125, 20);
+            this.totalBox.TabIndex = 19;
+            // 
+            // BillingApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 352);
+            this.Controls.Add(this.totalBox);
+            this.Controls.Add(this.calculateTotalButton);
+            this.Controls.Add(this.quantityBox5);
+            this.Controls.Add(this.quantityBox4);
+            this.Controls.Add(this.quantityBox3);
+            this.Controls.Add(this.quantityBox2);
+            this.Controls.Add(this.quantityBox1);
+            this.Controls.Add(this.priceBox5);
+            this.Controls.Add(this.priceBox4);
+            this.Controls.Add(this.priceBox3);
+            this.Controls.Add(this.priceBox2);
+            this.Controls.Add(this.priceBox1);
+            this.Controls.Add(this.descriptionBox5);
+            this.Controls.Add(this.descriptionBox4);
+            this.Controls.Add(this.descriptionBox3);
+            this.Controls.Add(this.descriptionBox2);
+            this.Controls.Add(this.descriptionBox1);
+            this.Controls.Add(this.quantityLabel);
+            this.Controls.Add(this.priceLabel);
+            this.Controls.Add(this.descriptionLabel);
+            this.Name = "BillingApp";
+            this.Text = "Billing Application";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label descriptionLabel;
+        private System.Windows.Forms.Label priceLabel;
+        private System.Windows.Forms.Label quantityLabel;
+        private System.Windows.Forms.TextBox descriptionBox1;
+        private System.Windows.Forms.TextBox descriptionBox2;
+        private System.Windows.Forms.TextBox descriptionBox3;
+        private System.Windows.Forms.TextBox descriptionBox4;
+        private System.Windows.Forms.TextBox descriptionBox5;
+        private System.Windows.Forms.TextBox priceBox1;
+        private System.Windows.Forms.TextBox priceBox2;
+        private System.Windows.Forms.TextBox priceBox3;
+        private System.Windows.Forms.TextBox priceBox4;
+        private System.Windows.Forms.TextBox priceBox5;
+        private System.Windows.Forms.TextBox quantityBox1;
+        private System.Windows.Forms.TextBox quantityBox2;
+        private System.Windows.Forms.TextBox quantityBox3;
+        private System.Windows.Forms.TextBox quantityBox4;
+        private System.Windows.Forms.TextBox quantityBox5;
+        private System.Windows.Forms.Button calculateTotalButton;
+        private System.Windows.Forms.TextBox totalBox;
+    }
+}
+
---
+++ BillingApplication/Form1.cs
@@ -0,0 +1,59 @@
+﻿//This application totals up the prices of 5 items in a shopping cart
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BillingApplication
+{
+    public partial class BillingApp : Form
+    {
+        public BillingApp()
+        {
+            InitializeComponent();
+        }
+
+        //Calculates and displays the total
+        private void calculateTotalButton_Click(object sender, EventArgs e)
+        {
+            double total = 0;
+            
+            //Make sure textboxes aren't empty
+            if (descriptionBox1.Text == "" || priceBox1.Text == "" || quantityBox1.Text == "" ||
+                descriptionBox2.Text == "" || priceBox2.Text == "" || quantityBox2.Text == "" ||
+                descriptionBox3.Text == "" || priceBox3.Text == "" || quantityBox3.Text == "" ||
+                descriptionBox4.Text == "" || priceBox4.Text == "" || quantityBox4.Text == "" ||
+                descriptionBox5.Text == "" || priceBox5.Text == "" || quantityBox5.Text == "")
+            {
+                MessageBox.Show("Please provide a full set of inputs: Product description, product price, and product quantity");
+            }
+            
+            else
+            {
+                //Check for bad inputs
+                try
+                {
+                    //Calculate total
+                    total = double.Parse(priceBox1.Text) * double.Parse(quantityBox1.Text)
+                        + double.Parse(priceBox2.Text) * double.Parse(quantityBox2.Text)
+                        + double.Parse(priceBox3.Text) * double.Parse(quantityBox3.Text)
+                        + double.Parse(priceBox4.Text) * double.Parse(quantityBox4.Text)
+                        + double.Parse(priceBox5.Text) * double.Parse(quantityBox5.Text);
+
+                    totalBox.Text = total.ToString("c");
+                }
+                
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Please only input numbers in the price and quantity boxes");
+                }
+            }
+        }
+    }
+}
